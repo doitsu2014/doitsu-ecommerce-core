ec1a536e03701f80e27e69c9523a4c6e9fcbf6f0
Duc Tran Huu <huuduc.tran@EITSVN.COM>
2019-10-10 15:07:05 +0700

    Comment some thing

--- Doitsu.Ecommerce.Core/Data/EcommerceDbContextFactory.cs
+++ Doitsu.Ecommerce.Core/Data/EcommerceDbContextFactory.cs
@@ -11,7 +11,10 @@ namespace Hris.Data.Identity
         public EcommerceDbContext CreateDbContext(string[] args)
         {
             var builder = new DbContextOptionsBuilder<EcommerceDbContext>();
+
+            // this line very important, to config the destination of Database to Migration
             var connectionString = "Server=103.114.104.24;Database=BachMoc_Furniture_Dev002;Trusted_Connection=False;User Id=sa;Password=zaQ@1234";
+            
             builder.UseSqlServer(connectionString);
             return new EcommerceDbContext(builder.Options);
         }
