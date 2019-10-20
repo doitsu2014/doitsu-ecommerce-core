414187ce6f8781b5f13ba1e194c99a78bdbd0aba
Huu Duc Tran <thd1152015@gmail.com>
2019-10-20 23:43:30 +0700

    Update more example connection on factory

--- Doitsu.Ecommerce.Core/Data/EcommerceDbContextFactory.cs
+++ Doitsu.Ecommerce.Core/Data/EcommerceDbContextFactory.cs
@@ -13,7 +13,8 @@ namespace Hris.Data.Identity
             var builder = new DbContextOptionsBuilder<EcommerceDbContext>();
 
             // this line very important, to config the destination of Database to Migration
-            var connectionString = "Server=localhost,1433;Database=BachMoc_Furniture_Dev;Trusted_Connection=False;User Id=sa;Password=zaQ@1234";
+            // var connectionString = "Server=localhost,1433;Database=BachMoc_Furniture_Dev;Trusted_Connection=False;User Id=sa;Password=zaQ@1234";
+            var connectionString = "Server=garden.dotvndns.vn,1444;Database=bachmoc_ver2_production;Trusted_Connection=False;User Id=bachmoc;Password=zaQ@1234";
             
             builder.UseSqlServer(connectionString);
             return new EcommerceDbContext(builder.Options);
