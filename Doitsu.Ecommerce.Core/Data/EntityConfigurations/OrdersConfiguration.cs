d35cbd50aa3d1d51b9cd86ca6bc53e2222f9c799
Huu Duc Tran <thd1152015@gmail.com>
2019-11-18 21:34:53 +0700

    Update dynamic attributes to order

--- Doitsu.Ecommerce.Core/Data/EntityConfigurations/OrdersConfiguration.cs
+++ Doitsu.Ecommerce.Core/Data/EntityConfigurations/OrdersConfiguration.cs
@@ -33,6 +33,16 @@ namespace Doitsu.Ecommerce.Core.Data.EntityConfigurations
 
             builder.Property(e => e.TotalPrice).HasColumnType("money");
 
+            builder.Property(e => e.Dynamic01).HasMaxLength(255);
+            
+            builder.Property(e => e.Dynamic02).HasMaxLength(255);
+
+            builder.Property(e => e.Dynamic03).HasMaxLength(255);
+            
+            builder.Property(e => e.Dynamic04).HasMaxLength(255);
+            
+            builder.Property(e => e.Dynamic05).HasMaxLength(1000);
+
             builder.HasOne(d => d.User)
                 .WithMany(p => p.Orders)
                 .HasForeignKey(d => d.UserId)
---
+++ Doitsu.Ecommerce.Core/Data/Migrations/20191118143332_InitAttributesToOrder.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Doitsu.Ecommerce.Core.Data.Migrations
+{
+    public partial class InitAttributesToOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Dynamic01",
+                table: "Orders",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Dynamic02",
+                table: "Orders",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Dynamic03",
+                table: "Orders",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Dynamic04",
+                table: "Orders",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Dynamic05",
+                table: "Orders",
+                maxLength: 1000,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dynamic01",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Dynamic02",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Dynamic03",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Dynamic04",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Dynamic05",
+                table: "Orders");
+        }
+    }
+}
