61e865042d86396abe191ce502b3279e57b9e57f
TRAN HUU DUC <thd1152015@gmail.com>
2020-06-05 00:37:53 +0700

    Fixbug

--- src/Doitsu.Ecommerce.Core/Utils.cs
+++ src/Doitsu.Ecommerce.Core/Utils.cs
@@ -10,7 +10,7 @@ namespace Doitsu.Ecommerce.Core
     {
         public class AppConstants
         {
-            public const string API_JWT_COOKIE = CookieAuthenticationDefaults.AuthenticationScheme + "," + API_JWT_AUTH_SCHEME + "," + OpenIdConnectDefaults.AuthenticationScheme;
+            public const string API_JWT_COOKIE = CookieAuthenticationDefaults.AuthenticationScheme + "," + API_JWT_AUTH_SCHEME;
             public const string API_JWT_AUTH_SCHEME = "api_jwt";
             public const string ADMIN_AUTH_SCHEME = "admin";
             
